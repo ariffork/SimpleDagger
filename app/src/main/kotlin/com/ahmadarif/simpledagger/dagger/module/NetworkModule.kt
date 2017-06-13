bbc944ecafec992fdf80329d2940074cffaffa62
Ahmad Arif <ahmad.arif019@gmail.com>
2017-06-13 20:07:14 +0700

    create NetworkModule

---
+++ app/src/main/kotlin/com/ahmadarif/simpledagger/dagger/module/NetworkModule.kt
@@ -0,0 +1,57 @@
+package com.ahmadarif.simpledagger.dagger.module
+
+import android.app.Application
+import com.ahmadarif.simpledagger.BuildConfig
+import dagger.Module
+import dagger.Provides
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ * Created by ARIF on 13-Jun-17.
+ */
+@Module
+class NetworkModule(val baseUrl: String) {
+
+    @Provides
+    @Singleton
+    fun cache(app: Application): Cache {
+        val cacheSize: Long = 10 * 1024 * 1024 // 10 MB
+        val cache = Cache(app.cacheDir, cacheSize)
+        return cache
+    }
+
+    @Provides
+    @Singleton
+    fun loggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        return interceptor
+    }
+
+    @Provides
+    @Singleton
+    fun httpClient(logger: HttpLoggingInterceptor, cache: Cache): OkHttpClient {
+        val builder = OkHttpClient().newBuilder()
+        builder.addInterceptor(logger)
+        builder.cache(cache)
+        return builder.build()
+    }
+
+    @Provides
+    @Singleton
+    fun retrofit(client: OkHttpClient) : Retrofit {
+        return Retrofit.Builder()
+                .client(client)
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+    }
+
+}
