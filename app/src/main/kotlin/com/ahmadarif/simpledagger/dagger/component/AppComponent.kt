5ed112de6df598d655891a49ba469c2ea6bf889c
Ahmad Arif <ahmad.arif019@gmail.com>
2017-06-13 20:22:02 +0700

    register modules to component

--- app/src/main/kotlin/com/ahmadarif/simpledagger/dagger/component/AppComponent.kt
+++ app/src/main/kotlin/com/ahmadarif/simpledagger/dagger/component/AppComponent.kt
@@ -1,6 +1,9 @@
 package com.ahmadarif.simpledagger.dagger.component
 
 import com.ahmadarif.simpledagger.activity.main.MainActivity
+import com.ahmadarif.simpledagger.dagger.module.ApiModule
+import com.ahmadarif.simpledagger.dagger.module.AppModule
+import com.ahmadarif.simpledagger.dagger.module.NetworkModule
 import dagger.Component
 import javax.inject.Singleton
 
@@ -8,7 +11,11 @@ import javax.inject.Singleton
  * Created by ARIF on 13-Jun-17.
  */
 @Singleton
-@Component(modules = arrayOf())
+@Component(modules = arrayOf(
+        AppModule::class,
+        NetworkModule::class,
+        ApiModule::class
+))
 interface AppComponent {
 
     fun inject(mainActivity: MainActivity)
